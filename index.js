6eb0cc4ca6f6a7f25979f8a26adcb0e2e172635c
Richard Marrujo <89800641+richardmarrujo@users.noreply.github.com>
2024-06-04 19:29:46 -0500

    Update index.js
    Added Sudden death and better calculations

--- index.js
+++ index.js
@@ -1,107 +1,123 @@
+var playerCount = 0;
+var playerNames = [];
+var started = false;
 
-var playerCount = 0
-var playerName = []
-var started = false
-var randomName = []
+// Prompt for number of players
+playerCount = parseInt(prompt("How many players? (Up to 4 players)"));
 
-// Prompt for how many players
-
-playerCount = prompt("How many players")
-
-// Prompt for name
-
-for (var i = 0; i < playerCount; i++) {
-  playerName.push(prompt("Player name?"));
+if (playerCount > 4) {
+  alert("The game can only accommodate up to 4 players.");
+  playerCount = 4;
 }
 
-// Replace placeholder for name
+// Prompt for player names
+for (var i = 0; i < playerCount; i++) {
+  playerNames.push(prompt("Enter name for Player " + (i + 1) + ":"));
+}
 
-$("#player1").text(playerName[0]);
-$("#player2").text(playerName[1]);
-$("#player3").text(playerName[2]);
-$("#player4").text(playerName[3]);
+// Function to create player dice elements
+function createPlayerDice() {
+  for (var i = 0; i < playerCount; i++) {
+    var newDice = `
+      <div class='dice'>
+        <p class='player${i + 1}'>${playerNames[i].toUpperCase()}</p>
+        <img class='img${i + 1}' src='images/dice6.png' alt='Dice image for Player ${i + 1}'>
+      </div>
+    `;
+    $("br").before(newDice);
+  }
+}
 
-// started???
+// Call the function to create player dice elements
+$(document).ready(function() {
+  createPlayerDice();
+});
+
+// Function to roll dice and determine winner
+function nextSequence() {
+  var rolls = [];
+  var randomNameIndex = Math.floor(Math.random() * playerCount);
+  var randomName = playerNames[randomNameIndex];
+
+  $("#start").text("Good Luck " + randomName + "!");
+
+  setTimeout(function() {
+    // Roll dice for each player
+    for (var i = 0; i < playerCount; i++) {
+      var randomNumber = Math.floor(Math.random() * 6) + 1;
+      var diceImage = "images/dice" + randomNumber + ".png";
+      $(".img" + (i + 1)).attr("src", diceImage);
+
+      rolls.push({ index: i, roll: randomNumber });
+    }
+
+    // Sort rolls in descending order
+    rolls.sort((a, b) => b.roll - a.roll);
+
+    // Enlarge the dice images based on roll results
+    for (var i = 0; i < rolls.length; i++) {
+      var size = (100 - (i * 20)) + "%";
+      $(".img" + (rolls[i].index + 1)).css("width", size);
+    }
+
+    // Check for ties and handle sudden death
+    var highestRoll = rolls[0].roll;
+    var winners = rolls.filter(player => player.roll === highestRoll);
+
+    if (winners.length > 1) {
+      $("#start").text("Sudden Death! Rolling again for " + winners.map(player => playerNames[player.index]).join(", ") + "...");
+      setTimeout(suddenDeath, 3000, winners.map(player => player.index));
+    } else {
+      $("#start").text("Good Luck " + randomName + "! The winner is " + playerNames[winners[0].index] + " with a roll of " + highestRoll + "!");
+    }
+  }, 1000); // 1-second delay
+}
+
+// Function to handle sudden death rounds
+function suddenDeath(tiedPlayers) {
+  var rolls = [];
+
+  for (var i = 0; i < tiedPlayers.length; i++) {
+    var playerIndex = tiedPlayers[i];
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    var diceImage = "images/dice" + randomNumber + ".png";
+    $(".img" + (playerIndex + 1)).attr("src", diceImage);
+
+    rolls.push({ index: playerIndex, roll: randomNumber });
+  }
+
+  // Sort rolls in descending order
+  rolls.sort((a, b) => b.roll - a.roll);
+
+  // Enlarge the dice images based on roll results
+  for (var i = 0; i < rolls.length; i++) {
+    var size = (100 - (i * 20)) + "%";
+    $(".img" + (rolls[i].index + 1)).css("width", size);
+  }
+
+  // Check for further ties and handle sudden death recursively if needed
+  var highestRoll = rolls[0].roll;
+  var winners = rolls.filter(player => player.roll === highestRoll);
+
+  if (winners.length > 1) {
+    $("#start").text("Another tie! Rolling again for " + winners.map(player => playerNames[player.index]).join(", ") + "...");
+    setTimeout(suddenDeath, 3000, winners.map(player => player.index));
+  } else {
+    $("#start").text("The winner is " + playerNames[winners[0].index] + " with a roll of " + highestRoll + "!");
+  }
+}
+
+// Event listener to start the game on any key press
 $(document).keypress(function() {
   if (!started) {
-    $("#start").text("Good Luck");
+    $(".dice").fadeIn();
+    $("#start").text("Good Luck!");
     nextSequence();
     started = true;
   }
 });
 
-// Make it add dice depending on playerCount
-
-// for (var i = 0; i < playerCount; i++) {
-//   playerCount[i]
-// }
-
-function nextSequence() {
-
-  var randomNumber = Math.floor(Math.random()*4);
-  var randomName = playerName[randomNumber];
-  console.log(randomName);
-
-  $("#start").text("Good Luck " + randomName +"!");
-
-  // First Dice Changer
-
-  var randomNumber1 = Math.floor(Math.random() *6) +1;
-
-  var randomDiceImage = "images/dice" +randomNumber1 + ".png";
-
-  var image1 = document.querySelectorAll("img")[0];
-
-  image1.setAttribute("src", randomDiceImage);
-
-  // Second Dice Changer
-
-  var randomNumber2 = Math.floor(Math.random() *6) +1;
-
-  var randomDiceImage = "images/dice" +randomNumber2 + ".png";
-
-  var image2 = document.querySelectorAll("img")[1];
-
-  image2.setAttribute("src", randomDiceImage);
-
-  // Third Dice Changer
-  var randomNumber3 = Math.floor(Math.random() *6) +1;
-
-  var randomDiceImage = "images/dice" +randomNumber3 + ".png";
-
-  var image3 = document.querySelectorAll("img")[2];
-
-  image3.setAttribute("src", randomDiceImage);
-
-  // Fourth Dice Changer
-  var randomNumber4 = Math.floor(Math.random() *6) +1;
-
-  var randomDiceImage = "images/dice" +randomNumber4 + ".png";
-
-  var image4 = document.querySelectorAll("img")[3];
-
-  image4.setAttribute("src", randomDiceImage);
-
-
-  // Calculations
-  setTimeout(function () {
-    if (randomNumber1 > randomNumber2 && randomNumber3 && randomNumber4){
-      $("#start").text(playerName[0] + " Wins! " + " Sorry " + randomName + " 😂 ")
-    }
-    if (randomNumber2 > randomNumber1 && randomNumber3 && randomNumber4) {
-      $("#start").text(playerName[1] + " Wins! " + " Sorry " + randomName + " 😂 " )
-    }
-    if (randomNumber3 > randomNumber1 && randomNumber2 && randomNumber4) {
-      $("#start").text(playerName[2] + " Wins! " + " Sorry " + randomName + " 😂 " )
-    }
-    if (randomNumber4 > randomNumber1 && randomNumber2 && randomNumber3) {
-      $("#start").text(playerName[3] + " Wins! " + " Sorry " + randomName + " 😂  ")
-    }
-    if (randomNumber1 === randomNumber2 && randomNumber2 === randomNumber3 && randomNumber1 === randomNumber1 && randomNumber4 === randomNumber4) {
-      document.querySelector("#start").innerHTML = "Draw!"
-    }
-  }, 1000);
-
-
-
-};
+// Event listener for the Roll The Dice button
+$(".refresh").click(function() {
+  nextSequence();
+});
